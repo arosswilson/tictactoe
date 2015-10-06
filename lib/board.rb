c457fbbe932fd0228aa8c394506a980d0d9eec40
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-10-06 10:40:43 -0400

    added board methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Board
   attr_accessor :board
   def initialize(args={})
@@ -8,23 +9,45 @@ class Board
   def possible_moves
     moves = []
     @board.each.with_index do |row, c_num|
-      row.each.with_index{|space, r_num| moves << "#{c_num.to_s},#{r_num.to_s}" if space =='_'}
+      row.each.with_index{|space, r_num|
+        moves << "#{c_num.to_s},#{r_num.to_s}" if space =='_'}
     end
     moves
   end
 
-  def to_s
-    counter = 1
-    display = (1..@board_size)
-    display = board.map do |row|
-      row.unshift(counter.to_s).join("|")+ "\n"
-      counter += 1
-    end
-    puts display.join('')
+  def move(marker, position)
+    pos = position.split(',')
+    return nil if board[pos[0]][pos[1]] !='_'
+    board[pos[0]][pos[1]] = marker
   end
+
+  def to_s
+    display = " " +('0'...(board_size.to_s).to_a.join(' ') + "\n"
+    board.each.with_index do |row, i|
+      display += (i+1).to_s + row.join("|")+ "\n"
+    end
+    puts display
+  end
+
+  def row_check(gameboard)
+    gameboard.each do |row|
+      return row[0] if row.uniq.length == 1 && !(row.include?('_'))
+    end
+  end
+
+  def diag_check(gameboard)
+    diag_one = (0...board_size).collect{|i| board[i][i]}
+    diag_two = (1..board_size).collect{|i| board[-i][(i-1)]}
+    return diag_one[0] if diag_one.uniq.length == 1 && !(diag_one.include?('_'))
+    return diag_two[0] if diag_two.uniq.length == 1 && !(diag_two.include?('_'))
+  end
+
 
 
 end
 
 b = Board.new({board_size: 3})
+puts b.possible_moves
 b.to_s
+puts b.possible_moves
+
